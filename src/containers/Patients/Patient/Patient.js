d8f3c074ca14f7196e8a244808173e6713a114d9
lucasbeto <skybilobran@gmail.com>
2020-06-27 16:12:46 -0300

    New patients page

--- src/containers/Patients/Patient/Patient.js
+++ src/containers/Patients/Patient/Patient.js
@@ -1,18 +1,8 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
-import { Button } from 'semantic-ui-react';
+import { Button, Grid, Divider, Item, List, GridColumn } from 'semantic-ui-react';
 import { sendAuthenticatedRequest } from '../../../utility/httpHelper';
 import Paginator from '../../../utility/paginator';
-import classes from './Patient.module.css';
-
-const mealMap = {
-    0: 'Café da manhã',
-    1: 'Lanche da manhã',
-    2: 'Almoço',
-    3: 'Lanche da tarde',
-    4: 'Jantar',
-    5: 'Lanche da noite',
-};
 
 class Patient extends Component {
   state = {
@@ -110,132 +100,139 @@ class Patient extends Component {
               {this.state.error ? <p>{this.state.error}</p> : null}
               {this.state.info ? (
                   <div>
-                      <h3>{this.state.info.name}</h3>
-                      <Button
-                        style={ { margin: '10px' } }
-                        color="teal"
-                        size="small"
-                        onClick={ () => this.props.history.push(`/pacientes/${params.id}/edit`) }
-                      >
-              Editar dados do paciente
-                      </Button>
-                      <p>
-Data de nascimento:
-                          {this.state.info.date_of_birth}
-                      </p>
-                      <p>
-              Sexo:
-                          {' '}
-                          {this.state.info.biological_sex === 0 ? 'Feminino' : 'Masculino'}
-                      </p>
-                      <p>
-              Etnia:
-                          {' '}
-                          {this.state.info.ethnic_group === 0
-                              ? 'Branco/Hispânico'
-                              : 'Afroamericano'}
-                      </p>
-                      <p>
-              Restrições alimentares:
-                          {' '}
-                          {this.state.info.food_restrictions.length === 0
-                              ? 'Não há'
-                              : this.state.info.food_restrictions.reduce(
-                                  (bigString, elem, index, arr) => bigString
-                      + elem.food_name
-                      + (index === arr.length - 1 ? '' : ', '),
-                                  '',
-                              )}
-                      </p>
-                  </div>
+                  <Grid style={ { margin: '20px' } }>
+                    <Grid.Row columns={ 1 }>
+                        <Button
+                          color="teal"
+                          size="small"
+                          onClick={ () => this.props.history.push('/pacientes') }
+                        >
+                        Pacientes
+                        </Button>
+                    </Grid.Row>
+                    <Divider />
+                    <Grid.Row style = { {textAlign: 'left' } } columns="equal">
+                        <Grid.Column></Grid.Column>
+                        <Grid.Column width={ 8 }>
+                        <Item >
+                            <Item.Content>
+                                <Item.Header>
+                                    <Grid columns='equal'>
+                                        <Grid.Column width={ 6 } >
+                                            <h3>{this.state.info.name}</h3>
+                                        </Grid.Column>
+                                        <Grid.Column width={ 10 }>
+                                            <Button.Group floated='right'>
+                                            <Button
+                                              color="teal"
+                                              size="mini"
+                                              onClick={ () => this.props.history.push(`/pacientes/${params.id}/edit`) }
+                                            >
+                                            Editar
+                                            </Button>
+                                            <Button
+                                              color="purple"
+                                              size="mini"
+                                              onClick={ this.deletePacient }
+                                            >
+                                            Excluir
+                                            </Button>
+                                            </Button.Group>
+                                        </Grid.Column>
+                                    </Grid>
+                                </Item.Header>
+                                <Item.Description>
+                                    <List horizontal>
+                                        <List.Item >
+                                            <List.Header>Data de nascimento</List.Header>{this.state.info.date_of_birth}
+                                        </List.Item>
+                                        <List.Item >
+                                            <List.Header>Sexo</List.Header>{this.state.info.biological_sex === 0 ? 'Feminino' : 'Masculino'}
+                                        </List.Item>
+                                        <List.Item >
+                                            <List.Header>Etnia</List.Header>{this.state.info.ethnic_group === 0 ? 'Branco/Hispânico' : 'Afroamericano'}
+                                        </List.Item>
+                                        <List.Item >
+                                            <List.Header>Restrições alimentares</List.Header>
+                                            {this.state.info.food_restrictions.length === 0
+                                                ? 'Não há'
+                                                : this.state.info.food_restrictions.reduce(
+                                                    (bigString, elem, index, arr) => bigString
+                                        + elem.food_name
+                                        + (index === arr.length - 1 ? '' : ', '),
+                                                    '',
+                                                )}
+                                        </List.Item>
+                                    </List>
+                                </Item.Description>
+                            </Item.Content>
+                        </Item>
+                        </Grid.Column>
+                        <Grid.Column></Grid.Column>
+                    </Grid.Row>
+                    <Divider />
+                  </Grid>
+                  </div>    
               ) : null}
-              <Button
-                style={ { margin: '10px' } }
-                color="teal"
-                size="small"
-                onClick={ () => this.props.history.push(
-                    `/pacientes/${params.id}/criar-ficha`,
-                ) }
-              >
-          Criar ficha para o paciente
-              </Button>
-              <Button
-                style={ { margin: '10px' } }
-                color="teal"
-                size="small"
-                onClick={ () => this.props.history.push(`/cardapio/${params.id}`) }
-              >
-          Criar cardápio para o paciente
-              </Button>
-              {this.state.menuInfo && this.state.menuInfo.map(menu => 
-                  (<div key={ menu.id } style={ {margin: 'auto', width: '20%', border: '1px solid black'} }>
-                      <h4>{ mealMap[menu.meal_type] }</h4>
-                <p>{menu.portions.reduce((prev, curr)=> prev+` ${curr.quantity} ${curr.food.food_name}`, '')}</p></div>))
-              }
-              <br />
-              {this.state.recordQueryInfo ? (
-                  <div className={ classes.records }>
-                      <Paginator
-                        queryResults={ this.state.recordQueryInfo }
-                        filter={ () => true }
-                        listElementMap={ (record) => (
-                              <div
-                                key={ record.id }
-                                onClick={ () => this.props.history.push(
-                                    `/pacientes/${params.id}/ficha/${record.id}`,
-                                ) }
-                                className={ classes.record }
-                              >
-                                  <p>
-                                      <span>
-Modificado em:
-                                          {record.date_modified}
-                                      </span>
-                                      <span>
-Peso:
-                                          {record.corporal_mass}
-                                      </span>
-                                      <span>
-Altura:
-                                          {record.height}
-                                      </span>
-                                      <span>
-IMC:
-                                          {record.BMI}
-                                      </span>
-                                  </p>
-                              </div>
-                        ) }
-                        setResults={ (recordInfo) => this.setState({ recordQueryInfo: recordInfo }) }
-                        setHasNext={ (value) => this.setState({ hasNext: value }) }
-                        setHasPrevious={ (value) => this.setState({ hasPrevious: value }) }
-                        setMessage={ (message) => this.setState({
-                            error: message,
-                        }) }
-                        hasPrevious={ this.state.hasPrevious }
-                        hasNext={ this.state.hasNext }
-                        buttonSize="large"
-                      />
-                  </div>
-              ) : (
-                  <p>Ainda não há uma ficha para esse paciente!</p>
-              )}
-              <Button
-                className={ classes.backButton }
-                color="teal"
-                size="medium"
-                onClick={ () => this.props.history.push('/pacientes') }
-              >
-          Voltar à página de pacientes
-              </Button>
-              <Button
-                style={ { margin: '200px auto' } }
-                color="red"
-                size="small"
-                onClick={ this.deletePacient }
-              >
-          Excluir paciente
-              </Button>
+              <Grid style={ { margin: '20px' } }>
+                    <Grid.Row style = { {textAlign: 'left' } } columns="equal">
+                        <Grid.Column></Grid.Column>
+                        <Grid.Column width={ 8 }>
+                            <Grid.Row>
+                                <Grid  columns="equal">
+                                    <GridColumn>
+                                    <h2 style = { {textAlign: 'left', marginTop: '10px' } }>Consultas</h2>
+                                    </GridColumn>
+                                    <GridColumn>
+                                    <Button
+                                      style={ { margin: '10px' } }
+                                      color="teal"
+                                      size="small"
+                                      onClick={ () => this.props.history.push(`/pacientes/${params.id}/criar-ficha`,) }
+                                    >
+                                    Nova Consulta
+                                    </Button>
+                                    </GridColumn>
+                                    <GridColumn width={ 6 }></GridColumn>
+                                </Grid>
+                            </Grid.Row>
+                            <Grid.Row>
+                                {this.state.recordQueryInfo ? (
+                                
+                                <Paginator
+                                  isConsultList
+                                  queryResults={ this.state.recordQueryInfo }
+                                  filter={ () => true }
+                                  listElementMap={ (record) => (
+                                    <div
+                                      key={ record.id }
+                                      onClick={ () => this.props.history.push(`/pacientes/${params.id}/ficha/${record.id}`,) }
+                                    >
+                                    <span>Data: {record.date_modified}</span>
+                                    <span>Peso:{record.corporal_mass}</span>
+                                    <span>Altura:{record.height}</span>
+                                    <span>IMC:{record.BMI}</span>
+                                    </div>
+                                  ) }
+                                  setResults={ (recordInfo) => this.setState({ recordQueryInfo: recordInfo }) }
+                                  setHasNext={ (value) => this.setState({ hasNext: value }) }
+                                  setHasPrevious={ (value) => this.setState({ hasPrevious: value }) }
+                                  setMessage={ (message) => this.setState({
+                                      error: message,
+                                  }) }
+                                  hasPrevious={ this.state.hasPrevious }
+                                  hasNext={ this.state.hasNext }
+                                  buttonSize="large"
+                                />
+                                
+                                ) : (<p>Ainda não há consultas para esse paciente!</p>)}
+                            </Grid.Row>
+                    </Grid.Column>
+                    <Grid.Column></Grid.Column>
+                    </Grid.Row>
+                </Grid>
+              
+              
               {this.state.redirectUrl && <Redirect to={ this.state.redirectUrl } />}
           </div>
       );
